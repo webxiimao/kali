84717eb8bf5fd97495c6eaf39152b3d842e1b49d
root <root@localhost.localdomain>
2018-08-27 20:16:18 +0800

    递归接受请求

--- 03-scrapy/meirenku/meirenku/spiders/meirenku.py
+++ 03-scrapy/meirenku/meirenku/spiders/meirenku.py
@@ -11,16 +11,32 @@ sys.setdefaultencoding('utf8')
 
 class Meirenku(scrapy.Spider):
         name = 'meirenku'
-        start_urls = ['http://huaban.com/boards/favorite/beauty/?jlc2lzkm&max=10000&limit=20&wfl=1']
+        current_id = '19768826'
+        start_urls = ['http://huaban.com/boards/favorite/beauty?jlc67zvd&max=19768826&limit=10&wfl=1']
+        base_url = 'http://huaban.com/boards/favorite/beauty?jlc67zvd&max='
+        count = 1
+        
+
+        # def start_requests(self):
+                
+
 
         def parse(self, response):
                 print '\n'
                 print json.loads(response.body)
                 print '\n'
-                text = response.body
+                text = json.loads(response.body)
+                self.current_id = text['boards'][-1]['board_id']
+                self.count += 1
                 # text = json.dumps(json.loads(response.body))
                 # text = json.dumps(dict(response.body), ensure_ascii = False) + ",\n"
-                f = open('meizijson.json', 'w')
-                f.write(text.encode('utf-8'))
+                f = open('meizijson%s.json'%(self.count), 'w')
+                f.write(response.body.encode('utf-8'))
                 f.close()
+                yield Request('%s%s&limit=10&wfl=1'%(self.base_url,self.current_id),callback=self.parse)
+
+        # def parse_next(self,response):
+        #         f = open('meizijson%s.json'%(self.count), 'w')
+        #         f.write(response.body.encode('utf-8'))
+        #         f.close()
                 
