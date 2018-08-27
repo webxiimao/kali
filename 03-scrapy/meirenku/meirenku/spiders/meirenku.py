4806b0924e46945a3a72d324d6108752f147994a
root <root@localhost.localdomain>
2018-08-27 20:37:37 +0800

    完成二级页面

--- 03-scrapy/meirenku/meirenku/spiders/meirenku.py
+++ 03-scrapy/meirenku/meirenku/spiders/meirenku.py
@@ -13,13 +13,15 @@ class Meirenku(scrapy.Spider):
         name = 'meirenku'
         current_id = '19768826'
         start_urls = ['http://huaban.com/boards/favorite/beauty?jlc67zvd&max=19768826&limit=10&wfl=1']
+        # start_urls = ['http://huaban.com/boards/24383531/']
+                                        #http://huaban.com/boards/24383531/?jlc8afac&max=824526922&limit=20&wfl=1      
         base_url = 'http://huaban.com/boards/favorite/beauty?jlc67zvd&max='
         count = 1
         
 
         # def start_requests(self):
                 
-
+       
 
         def parse(self, response):
                 print '\n'
@@ -28,15 +30,18 @@ class Meirenku(scrapy.Spider):
                 text = json.loads(response.body)
                 self.current_id = text['boards'][-1]['board_id']
                 self.count += 1
-                # text = json.dumps(json.loads(response.body))
-                # text = json.dumps(dict(response.body), ensure_ascii = False) + ",\n"
+                # f = open('meizijson%s.json'%(self.count), 'w')
+                # f.write(response.body.encode('utf-8'))
+                # f.close()
+                for board in text['boards']:
+                        print '\n'
+                        print('http://huaban.com/boards/%s/'%(str(board['board_id'])))
+                        print '\n'
+                        yield Request('http://huaban.com/boards/%s/'%(str(board['board_id'])), callback=self.parse_next)
+                # yield Request('%s%s&limit=10&wfl=1'%(self.base_url,self.current_id),callback=self.parse)
+
+        def parse_next(self,response):
                 f = open('meizijson%s.json'%(self.count), 'w')
                 f.write(response.body.encode('utf-8'))
                 f.close()
-                yield Request('%s%s&limit=10&wfl=1'%(self.base_url,self.current_id),callback=self.parse)
-
-        # def parse_next(self,response):
-        #         f = open('meizijson%s.json'%(self.count), 'w')
-        #         f.write(response.body.encode('utf-8'))
-        #         f.close()
                 
